bf4aa03fd74e7d854312b299067b9357066beb61
Pawan Gautam <pawangtm2419@gmail.com>
2021-08-20 18:16:46 +0530

    menifest and service worker

--- incs/header.php
+++ incs/header.php
@@ -1,7 +1,7 @@
 <div class="header_bg">
-  <div class="container">
+  <div class="container-fluid">
     <div class="header">
-      <div class="logo col-md-4"> <a href="index.php"><img src="images/logo.png" alt=""/> </a> </div>
+      <div class="logo col-md-4"> <a href="index.php"><img src="images/logo.png" alt=""> </a> </div>
       <!-- start header_right -->
       <div class="header_right col-md-8">
         <div class="col-md-12 right-header">
@@ -9,14 +9,19 @@
           <div class="col-md-10 header-links">
             <ul>
               <li><a href="#">
-                <?php if($_SESSION['custFname']){ echo "Welcome -"; echo $_SESSION['custFname'];} ?>
+                  <?php 
+                    if($_SESSION['custFname']) { echo "Welcome -"; echo $_SESSION['custFname']; } 
+                  ?>
                 </a></li>
               <?php
                 $FotrQuery = mysql_query("select * from pgs where manucatg='headerTop' order by id desc LIMIT 3");
-                 while($FotrQuery1 = mysql_fetch_array($FotrQuery))
-                 {?>
-              <li><a href="pages.php?pgid=<?php echo $FotrQuery1['id']; ?>"><?php echo str_replace('_', ' ', $FotrQuery1['pagenmae']); ?></a></li>
-              <?php }?>
+                 while($FotrQuery1 = mysql_fetch_array($FotrQuery)) {
+                   ?>
+                    <li>
+                      <a href="pages.php?pgid=<?php echo $FotrQuery1['id']; ?>"><?php echo str_replace('_', ' ', $FotrQuery1['pagenmae']); 
+                      ?>
+                      </a></li>
+                    <?php }?>
               <?php
 			   if($_SESSION['custEmail']){
                echo "<li><a href='myaccount.php'>My Account</a></li>";
@@ -29,7 +34,9 @@
               // echo "<li><a href='login.php'>Login</a></li>";
 			   }
 			  ?>
-              <li><a href="#" class="currency"> <?php echo $_SESSION['man']; ?></a></li>
+              <li><a href="#" class="currency">
+                  <?php echo $_SESSION['man']; ?>
+                </a></li>
             </ul>
           </div>
           <div class="col-md-2 language" style="display:none;">
@@ -42,22 +49,30 @@
           <div class="col-md-8 search">
             <form name="frm-search" action="product-search.php" method="post" enctype="multipart/form-data">
               <div class="col-md-9 search-box nopadding">
-                <input type="search" name="srchkey" class="form-control" placeholder="Search Products " />
+                <input type="search" name="srchkey" class="form-control" placeholder="Search Products ">
               </div>
               <div class="col-md-3 search-btn nopadding">
                 <button type="submit" name="filter" class="btn btn-primary"><i class="fa fa-search"></i> SEARCH</button>
               </div>
             </form>
           </div>
-          <div class="col-md-3 nopadding registers pull-right">
-          <div class="social-icons">
-          <a href="#" target="_blank"><img src="http://truetimetechnology.com/images/social/insta.png" alt="Instagram" title="facebook"></a>
-                      <a href="#" target="_blank"><img src="images/social/fb.jpg" alt="facebook" title="facebook"></a>             <a href="#" target="_blank"><img src="images/social/twitter.jpg" alt="facebook" title="facebook"></a>             <a href="#" target="_blank"><img src="images/social/linkdin.PNG" alt="facebook" title="facebook"></a>    			</div>
-                       </div>
+          <div class="col-md-2 nopadding registers pull-right">
+            <div class="social-icons">
+              <a href="#" target="_blank"><img src="http://truetimetechnology.in/images/social/insta.png"
+                  alt="Instagram" title="facebook"></a>
+              <a href="#" target="_blank"><img src="images/social/fb.jpg" alt="facebook" title="facebook"></a>
+              <a href="#" target="_blank"><img src="images/social/twitter.jpg" alt="facebook" title="facebook"></a>
+              <a href="#" target="_blank"><img src="images/social/linkdin.PNG" alt="facebook" title="facebook"></a>
+            </div>
+          </div>
           <div class="col-md-2 nopadding registers pull-right countItems" style="display:none;">
-          <div class="social-icons">
-                      <a href="#" target="_blank"><img src="images/social/fb.jpg" alt="facebook" title="facebook"></a>             <a href="#" target="_blank"><img src="images/social/twitter.jpg" alt="facebook" title="facebook"></a>             <a href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><img src="images/social/linkdin.PNG" alt="facebook" title="facebook"></a>    			</div>
-                       </div>
+            <div class="social-icons">
+              <a href="#" target="_blank"><img src="images/social/fb.jpg" alt="facebook" title="facebook"></a> <a
+                href="#" target="_blank"><img src="images/social/twitter.jpg" alt="facebook" title="facebook"></a> <a
+                href="https://www.linkedin.com/home?trk=nav_responsive_tab_home" target="_blank"><img
+                  src="images/social/linkdin.PNG" alt="facebook" title="facebook"></a>
+            </div>
+          </div>
           <div class="cartbos" style="display:none;"> </div>
         </div>
         <!--end right search +register -->
@@ -67,42 +82,50 @@
   </div>
   <!-- start header menu -->
   <div class="menu-area">
-  <div class="container">
-  <ul class="megamenu skyblue">
-    <li><a href="index.php">Home</a></li>
-    <?php 
+    <div class="container-fluid">
+      <ul class="megamenu skyblue">
+        <li><a href="index.php">Home</a></li>
+        <?php 
 		    $selectcategory = mysql_query("select *from product group by category");
 			while($selectcategory1 = mysql_fetch_array($selectcategory))
 			{ 
 			$carts = $selectcategory1['category'];
 			?>
-    <li class="grid"> 
-    <a style="width:100%;" href="products.php?catg=<?php echo $selectcategory1['category'];?>"><?php echo str_replace("_"," ",$selectcategory1['category']); ?></a>
-      <!--dropdown-->
-      <?php  echo "<div class='megapanel'><div class='row'>";  ?>
-      <div class="col1" style="width:100%;">
-      <div class="h_nav" style="width:100%;">
-      <?php 
+        <li class="grid">
+          <a style="width:100%;" href="products.php?catg=<?php echo $selectcategory1['category'];?>">
+            <?php echo str_replace("_"," ",$selectcategory1['category']); ?>
+          </a>
+          <!--dropdown-->
+          <?php  echo "<div class='megapanel'><div class='row'>";  ?>
+          <div class="col1" style="width:100%;">
+            <div class="h_nav" style="width:100%;">
+              <?php 
 						echo "<ul style='width:100%;'>"; 
 					    $selectsubcategory = mysql_query("select distinct category,sub_category,title_url from product where category = '$carts'");
 						while($selectsubcategory1 = mysql_fetch_array($selectsubcategory))
 						{ ?>
-    <li><a href="products.php?catg=<?php echo $selectsubcategory1['category'];?>&&subcatg=<?php echo $selectsubcategory1['sub_category'];?>&&titleurl=<?php echo $selectsubcategory1['title_url'];?>" style="width:100%;"><?php echo str_replace("_"," ",$selectsubcategory1['sub_category']); ?></a></li>
-    <?php } echo "</ul>"; ?>
+        <li><a
+            href="products.php?catg=<?php echo $selectsubcategory1['category'];?>&&subcatg=<?php echo $selectsubcategory1['sub_category'];?>&&titleurl=<?php echo $selectsubcategory1['title_url'];?>"
+            style="width:100%;">
+            <?php echo str_replace("_"," ",$selectsubcategory1['sub_category']); ?>
+          </a></li>
+        <?php } echo "</ul>"; ?>
     </div>
-    </div>
-    <?php echo "</div></div>";?>
-    <!--end dropdown-->
-    </li>
-    <?php } ?>
-    <?php
+  </div>
+  <?php echo "</div></div>";?>
+  <!--end dropdown-->
+  </li>
+  <?php } ?>
+  <?php
 		    $menuCat ="main-manu";
 		    $selectmenupgs = mysql_query("select * from pgs where manucatg='$menuCat' AND status = '1' order by id asc LIMIT 2");
 			 while($selectmenupgs1 = mysql_fetch_array($selectmenupgs))
 			 {?>
-    <li><a href="pages.php?pgid=<?php echo $selectmenupgs1['id']; ?>"><?php echo $selectmenupgs1['pagenmae']; ?></a></li>
-    <?php } ?>
-    <li><a href="contact-us.php">Contact Us</a></li>
+  <li><a href="pages.php?pgid=<?php echo $selectmenupgs1['id']; ?>">
+      <?php echo $selectmenupgs1['pagenmae']; ?>
+    </a></li>
+  <?php } ?>
+  <li><a href="contact-us.php">Contact Us</a></li>
   </ul>
 </div>
 </div>
--- index.php
+++ index.php
@@ -7,177 +7,150 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-
-<title>
-  <?php if($seoQuery1){ echo $seoQuery1['title']; } else { echo "True Time Technology";}?>
-</title>
-<meta name="description" content="<?php echo $seoQuery1['description']; ?>" />
-<meta name="keywords" content="<?php echo $seoQuery1['keyword']; ?>" />
-<link rel="Shortcut Icon" href="favicon.ico" type="image/x-icon" />
-<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-<!-- jQuery (necessary JavaScript plugins) -->
-<script type='text/javascript' src="<?php echo $url; ?>js/jquery-1.11.1.min.js"></script>
-<!-- Custom Theme files -->
-<link href="<?php echo $url; ?>css/style.css" rel='stylesheet' type='text/css' />
-<!-- Custom Theme files -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- font awesome -->
-<link href="<?php echo $url; ?>css/font-awesome.css" rel="stylesheet">
-<link href="<?php echo $url; ?>css/font-awesome.min.css" rel="stylesheet">
-<!-- start menu -->
-<link href="<?php echo $url; ?>css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
-<!-- start slider -->
-<link rel="stylesheet" href="<?php echo $url; ?>css/flexslider.css" media="all">
-<!-- start carousel -->
-<link type="text/css" rel="stylesheet" href="<?php echo $url; ?>css/owl.carousel.css" media="screen" />
-<!-- start menu -->
-
-<script src="<?php echo $url; ?>js/menu_jquery.js"></script>
-<script type="text/javascript" src="<?php echo $url; ?>js/megamenu.js"></script>
- 
-<!-- start carousel -->
-<script type="text/javascript" src="<?php echo $url; ?>js/owl.carousel.js"></script>
-<!-- custom -->
-<script src="<?php echo $url; ?>js/jquery-ui.min.js"></script>
-<script src="js/scripts.js"></script>
-<?php require_once("$url"."incs/scripts.php"); ?>
-
-<!--language -->
-<script type="text/javascript">
-function googleTranslateElementInit() {
- new google.translate.TranslateElement({
-   pageLanguage: 'en',
-   includedLanguages: 'af,sq,ar,hy,az,eu,be,bg,ca,zh-CN,zh-TW,hr,cs,da,nl,en,et,tl,fi,fr,gl,ka,de,el,ht,iw,hi,hu,is,id,ga,it,ja,ko,lv,lt,mk,ms,mt,no,fa,pl,pt,ro,ru,sr,sk,sl,es,sw,sv,th,tr,uk,ur,vi,cy,yi'
- }, 'google_translate_element');
-}
-</script>
-
-<script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-</head>
-<body>
-<!-- header -->
-<?php require_once("$url"."incs/header.php"); ?>
-<!--end header-->
-<!-- content -->
-<div class="container-fluid">
-  <div class="main">
-    <div class="content_top">
-      <?php //require_once("$url"."incs/left-sidebar.php"); ?>
-      <div class="main-content">
-      <div class="col-md-12" style="padding:5px 0;">
-       <div class="col-md-9" style="padding:0;">
-        <!-- start slider -->
-        <div class="flexslider">
-          <ul class="slides">
-			   <?php
-                $selectbanner = mysql_query("select * from banner where status = '1' order by id desc");
-                while($selectbanner1 = mysql_fetch_array($selectbanner))
-                {
-                ?>  
-                 <li> <img src="images/banner/800+400_<?php echo $selectbanner1['banner'];?>" alt="">
-                   <!-- <div class="desc">
-                      <div class="container">
-                        <h2 class="title">Outsourced Marketing Department for the MSME's.</h2>
-                        <p>- We help you overcome the new challenges in Sales & Marketing.</p>
-                      </div>
-                    </div>-->
-                  </li>          
-          <?php } ?>
-          </ul>
-         </div>
-        <!-- end  slider -->
-       </div>
-       <div class="col-md-3" style="padding:0;">
-         <?php
-			$selectadvertise = mysql_query("select * from advertise where status = '1' order by id asc");
-			while($selectadvertise1 = mysql_fetch_array($selectadvertise))
-			{
-			?>
-             <!--ads-->
-              <div class="ads">
-               <a href="#">
-                <img src="images/ads/<?php echo $selectadvertise1['image'];?>" alt="ads" />
-               </a>
-              </div>
-              <!--end ads-->
-			<?php } ?>
-       </div>
-       </div>
-        <!--- ================== content ================== -->
-        <div class="contents">
-          <!--about -->
-          
-          
-          <!--end about -->
-          <form name="frm-product" action="" method="post" enctype="multipart/form-data">
-          <?php 
-          $proCat = mysql_query("select distinct category from product where hstatus = '1' order by id asc");
-		  if(empty($proCat))
-		  {echo "No Data Found !";}else{
-          while($proCat1 = mysql_fetch_array($proCat))
-          {
-          $catg = $proCat1['category'];
-          ?>
-          <!--carsual item-->
-          <div class="products">
-          
-            <h3 class="title-heading"><i class="fa fa-bars green-icons"></i> <?php echo str_replace('_', ' ', $catg); ?></h3>
-            <div class="owl-portfolio owl-carousel">
-              <?php
-                 $proQuery = mysql_query("select * from product where category = '$catg' order by id desc");
-                 while($proQuery1 = mysql_fetch_array($proQuery))
-                 {
-                 $proImg = str_replace(' ', '_', $proQuery1['sub_category']);
-                 ?>
-              <!--item-->
-              <div class="item"> <a href="product-detail.php?product_id=<?php echo $proQuery1['id'];?>">
-                <div class="entry"> 
-                 <img src="images/products/<?php echo $proQuery1['category'];?>/<?php echo $proImg;?>/250+250_<?php echo $proQuery1['product_image'];?>" class="proImg" data-src="images/products/<?php echo $proQuery1['category'];?>/<?php echo $proImg;?>/250+250_<?php echo $proQuery1['product_image'];?>" alt=""> 
-                </div>
-                </a>
-                <div class="image-caption">
-                  <h3 class="owl-title">
-                   <a href="product-detail.php?product_id=<?php echo $proQuery1['id'];?>"><?php echo $proQuery1['product_name'];?></a>
-                  </h3>
-                  <div class="meta">
-                    
-                     <div class="price"><i class="fa fa-usd"></i> <?php echo number_format($proQuery1['sale_price'], 2);?></div>
-                     <div class="regprice"><i class="fa fa-usd"></i> <?php echo number_format($proQuery1['reg_price'], 2);?></div>
-                  </div>  
-                  <div class="owl-carousel-btn">
-                   <a href="contact-us.php" class="btn read-more btn-sm btn-primary"style="color: #fff;background: #106e9c;">Get quote</a>
+  <link rel="manifest" href="manifest.json">
+  <title><?php if($seoQuery1){ echo $seoQuery1['title']; } else { echo "True Time Technology";}?></title>
+  <meta name="description" content="<?php echo $seoQuery1['description']; ?>">
+  <meta name="keywords" content="<?php echo $seoQuery1['keyword']; ?>">
+  <link rel="Icon" href="favicon.ico" type="image/x-icon">
+  <link href="css/bootstrap.css" rel='stylesheet' type='text/css'>
+  <script type='text/javascript' src="<?php echo $url; ?>js/jquery-1.11.1.min.js"></script>
+  <link href="<?php echo $url; ?>css/style.css" rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="<?php echo $url; ?>css/font-awesome.css" rel="stylesheet">
+  <link href="<?php echo $url; ?>css/font-awesome.min.css" rel="stylesheet">
+  <link href="<?php echo $url; ?>css/megamenu.css" rel="stylesheet" type="text/css" media="all">
+  <link rel="stylesheet" href="<?php echo $url; ?>css/flexslider.css" media="all">
+  <link type="text/css" rel="stylesheet" href="<?php echo $url; ?>css/owl.carousel.css" media="screen">
+  <script src="<?php echo $url; ?>js/menu_jquery.js"></script>
+  <script type="text/javascript" src="<?php echo $url; ?>js/megamenu.js"></script>
+  <script type="text/javascript" src="<?php echo $url; ?>js/owl.carousel.js"></script>
+  <script src="<?php echo $url; ?>js/jquery-ui.min.js"></script>
+  <script src="js/scripts.js"></script>
+  <?php require_once("$url"."incs/scripts.php"); ?>
+  <script type="text/javascript">
+  function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    includedLanguages: 'af,sq,ar,hy,az,eu,be,bg,ca,zh-CN,zh-TW,hr,cs,da,nl,en,et,tl,fi,fr,gl,ka,de,el,ht,iw,hi,hu,is,id,ga,it,ja,ko,lv,lt,mk,ms,mt,no,fa,pl,pt,ro,ru,sr,sk,sl,es,sw,sv,th,tr,uk,ur,vi,cy,yi'
+  }, 'google_translate_element');
+  }
+  </script>
+  <script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+  </head>
+  <body>
+  <!-- header -->
+  <?php require_once("$url"."incs/header.php"); ?>
+  <div class="container-fluid">
+    <div class="main">
+      <div class="content_top">
+        <?php //require_once("$url"."incs/left-sidebar.php"); ?>
+        <div class="main-content">
+        <div class="col-md-12" style="padding:5px 0;">
+        <div class="col-md-9" style="padding:0;">
+          <!-- start slider -->
+          <div class="flexslider">
+            <ul class="slides">
+          <?php
+            $selectbanner = mysql_query("select * from banner where status = '1' order by id desc");
+            while($selectbanner1 = mysql_fetch_array($selectbanner)) {
+            ?>  
+              <li> <img src="images/banner/800+400_<?php echo $selectbanner1['banner'];?>" alt="">
+                <!-- <div class="desc">
+                  <div class="container">
+                    <h2 class="title">Outsourced Marketing Department for the MSME's.</h2>
+                    <p>- We help you overcome the new challenges in Sales & Marketing.</p>
                   </div>
-                </div>
-                <!-- image caption -->
-              </div>
-              <!--/ item -->
-              <?php } ?>
-            </div>
+                </div>-->
+              </li>          
+            <?php } ?>
+            </ul>
           </div>
-          <!--end carsual item-->
-          </form>
-          <?php }} ?>
+          <!-- end  slider -->
         </div>
-        <!--- ================== end content ================== -->
+        <div class="col-md-3" style="padding:0;">
+          <?php
+              $selectadvertise = mysql_query("select * from advertise where status = '1' order by id asc");
+              while($selectadvertise1 = mysql_fetch_array($selectadvertise)) {
+            ?>
+              <!--ads-->
+                <div class="ads">
+                <a href="#">
+                  <img src="images/ads/<?php echo $selectadvertise1['image'];?>" alt="ads">
+                </a>
+                </div>
+                <!--end ads-->
+        <?php } ?>
+        </div>
+        </div>
+          <div class="contents">
+            <form name="frm-product" action="" method="post" enctype="multipart/form-data">
+            <?php 
+              $proCat = mysql_query("select distinct category from product where hstatus = '1' order by id asc");
+              if(empty($proCat)) {
+                echo "No Data Found !";
+              } else {
+                while($proCat1 = mysql_fetch_array($proCat)) {
+                $catg = $proCat1['category'];
+              ?>
+            <!--carsual item-->
+            <div class="products">          
+              <h3 class="title-heading"><i class="fa fa-bars green-icons"></i> <?php echo str_replace('_', ' ', $catg); ?></h3>
+              <div class="owl-portfolio owl-carousel">
+                <?php
+                  $proQuery = mysql_query("select * from product where category = '$catg' order by id desc");
+                  while($proQuery1 = mysql_fetch_array($proQuery))
+                  {
+                  $proImg = str_replace(' ', '_', $proQuery1['sub_category']);
+                  ?>
+                <!--item-->
+                <div class="item"> <a href="product-detail.php?product_id=<?php echo $proQuery1['id'];?>">
+                  <div class="entry"> 
+                  <img src="images/products/<?php echo $proQuery1['category'];?>/<?php echo $proImg;?>/250+250_<?php echo $proQuery1['product_image'];?>" class="proImg" data-src="images/products/<?php echo $proQuery1['category'];?>/<?php echo $proImg;?>/250+250_<?php echo $proQuery1['product_image'];?>" alt=""> 
+                  </div>
+                  </a>
+                  <div class="image-caption">
+                    <h3 class="owl-title">
+                    <a href="product-detail.php?product_id=<?php echo $proQuery1['id'];?>"><?php echo $proQuery1['product_name'];?></a>
+                    </h3>
+                    <div class="meta">
+                      
+                      <div class="price"><i class="fa fa-usd"></i> <?php echo number_format($proQuery1['sale_price'], 2);?></div>
+                      <div class="regprice"><i class="fa fa-usd"></i> <?php echo number_format($proQuery1['reg_price'], 2);?></div>
+                    </div>  
+                    <div class="owl-carousel-btn">
+                    <a href="contact-us.php" class="btn read-more btn-sm btn-primary"style="color: #fff;background: #106e9c;">Get quote</a>
+                    </div>
+                  </div>
+                  <!-- image caption -->
+                </div>
+                <!--/ item -->
+                <?php } ?>
+              </div>
+            </div>
+            <!--end carsual item-->
+            </form>
+            <?php }} ?>
+          </div>
+          <!--- ================== end content ================== -->
+        </div>
+        <div class="clearfix"></div>
       </div>
-      <div class="clearfix"></div>
     </div>
   </div>
-</div>
-<!-- footer_top -->
-<?php require_once("$url"."incs/footer.php"); ?>
-<!-- start slider -->
-<script src="<?php echo $url; ?>js/jquery.flexslider.js"></script>
-<script type="text/javascript">
-  $('.flexslider').flexslider({
-	animation: "fade",
-	controlNav: true,
-	start: function(slider){
-	  $('body').removeClass('loading');
-	}
-  });
- </script>
+  <!-- footer_top -->
+  <?php require_once("$url"."incs/footer.php"); ?>
+  <!-- start slider -->
+  <script src="<?php echo $url; ?>js/jquery.flexslider.js"></script>
+  <script type="text/javascript">
+    $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: true,
+    start: function(slider) {
+      $('body').removeClass('loading');
+    }
+    });
+  </script>
+  <script src="js/main.js"></script>
 
-</body>
-</h
+  </body>
+</html>
